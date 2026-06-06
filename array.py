13fa285d0a400a6ecb9fcaf951f954cef1ebe797
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-06-06 18:54:01 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,17 @@
+num = [10, 20, 30,40,50]
+nums = ["sda", "adsd", "adasa"]
+print(type(num))
+
+
+a = int(input("Enter a number A: "))
+b = int(input("Enter a number B: "))
+
+if (a == b):
+    print("both numbers are same")
+elif (a > b):
+    print("A is greater than B")
+elif (a < b):
+    print("A is less than B")   
+else:
+    print("both numbers are not same")
+
---
+++ sample.py
@@ -0,0 +1,14 @@
+id = 10
+name = "vignesh"
+isAvailable = True
+
+print(id)
+print(type(id))
+print(name)
+print(isAvailable)
+
+a = 10
+b = 20
+
+c = a/b
+print(c)
