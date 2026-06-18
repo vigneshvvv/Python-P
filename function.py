518eb330d3b5c2e4099aab59e2bbecf29fa9b198
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-06-18 09:02:47 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,17 @@
+def printSample():
+    print("Sample function printing")
+
+
+printSample()
+printSample()
+
+def add(a,b,c):
+    print(a+b+c)
+
+add(10,100,50)
+
+def addition(a,b):
+    return a+b
+
+c = addition(150,250)
+print(c)
